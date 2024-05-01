4bc099906ba0d0bb3507cf3b118964a955f28ee5
Mayank Sethi <mayank@digibrain.co.in>
2024-05-01 20:52:41 +0530

    bugfix - global css file config and implmenent stying to banner

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
         />
-        <link rel="stylesheet" href="css/globals.css" />
+        <link rel="stylesheet" href="/css/globals.css" />
       </Head>
       <body>
         <div className="container">
