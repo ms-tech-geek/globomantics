db862ae6295cdff0cf6656dd1de6db703cbef2c1
Mayank Sethi <mayank@digibrain.co.in>
2024-05-01 20:29:39 +0530

    initial commit

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+          <link rel="stylesheet" href="css/globals.css"></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
