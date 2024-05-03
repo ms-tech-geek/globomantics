4482d8b2985f9340dbac6ca9bff3588997bfcca0
Mayank Sethi <mayank@digibrain.co.in>
2024-05-03 13:04:41 +0530

    created new component with currencyFormatter and modified price in houseRow in a new format

--- components/houseRow.js
+++ components/houseRow.js
@@ -1,9 +1,11 @@
+import currencyFormatter from '@/helpers/currencyFormatter.js';
+
 const HouseRow = ({ house: { address, country, price } }) => {
   return (
     <tr>
       <td>{address}</td>
       <td>{country}</td>
-      <td>{price}</td>
+      <td>{currencyFormatter.format(price)}</td>
     </tr>
   );
 };
