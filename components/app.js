1416ccc1268632bb3a5cb6d085c406b89ebfe245
Mayank Sethi <mayank@digibrain.co.in>
2024-05-03 12:23:58 +0530

    reuse components and destructure props

--- components/app.js
+++ components/app.js
@@ -1,7 +1,12 @@
 import Banner from './banner';
 
 const App = () => {
-  return <Banner headerText={'Providing houser all over the world'} />;
+  return (
+    <div>
+      <Banner headerText={'Providing houser all over the world'} />;
+      <Banner headerText={'Some other header text'} />;
+    </div>
+  );
 };
 
 export default App;
--- components/banner.js
+++ components/banner.js
@@ -6,14 +6,14 @@ const subtitleStyle = {
   color: 'coral',
 };
 
-const Banner = (props) => {
+const Banner = ({ headerText }) => {
   return (
     <header className="row mb-4">
       <div className="col-5">
         <img src="./GloboLogo.png" alt="logo" className={logo} />
       </div>
       <div className="col-7 mt-5" style={subtitleStyle}>
-        {props.headerText}
+        {headerText}
       </div>
     </header>
   );
